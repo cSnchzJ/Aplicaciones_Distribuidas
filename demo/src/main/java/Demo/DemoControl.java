8d282d981b060f5688327c4deaad71f4090e51c3
Diego Emilio Morales Rodriguez <55663503+ChenchoSykes182@users.noreply.github.com>
2024-06-04 11:31:44 -0600

    Add files via upload

---
+++ demo/src/main/java/Demo/DemoControl.java
@@ -0,0 +1,15 @@
+package Demo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi")
+@RequiredArgsConstructor
+public class DemoControl {
+    @PostMapping(value="demo")
+    public String welcome(){
+        return "Welcome form secure endpoint";
+    }
+}
